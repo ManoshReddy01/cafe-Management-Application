2ba1f39ff6fdd127b22a5cf7a5fc496140a98c31
ManoshReddy01 <gandramanoshreddy@gmail.com>
2026-06-27 19:17:50 +0530

    Updated frontend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 // Local Spring Boot Backend
-export const BASE_URL = 'http://localhost:9000/api';
+export const BASE_URL = "https://mrcoffee-backend.onrender.com/api";
 
 // ======================
 // AUTH APIs
